d46900d3b3368a42bbb29b2b22d769506511dea1
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-09-25 23:58:04 +0530

    v2 form completed

--- Customer.java
+++ Customer.java
@@ -1,14 +1,41 @@
 package com.student.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
+	
 	private String firstName;
 	
 	@NotNull(message="is required!!")
 	@Size(min=2,message="is required!!")
 	private String lastName;
+	// free passes detail
+	@Min(value=0,message="enter number greater than -1")
+	@Max(value=10,message="enter number less than 11")
+	private int freePasses;
+	
+	// enter postal code
+	@Pattern(regexp="^[a-zA-Z0-9]{5}",message="enter 5 digit/char")
+	private String postalCode;	
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+	
 	
 	public String getFirstName() {
 		return firstName;
